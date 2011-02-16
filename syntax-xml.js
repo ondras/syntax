389df4372677405204bb097e9b2ef6aca9550ebe
ondras <none@none>
2011-02-16 08:37:25 +0100

    s/html/xml/
    --HG--
    rename : syntax-html.js => syntax-xml.js

--- syntax-xml.js
+++ syntax-xml.js
@@ -3,13 +3,13 @@
 	
 	patterns.push({
 		token: "comment",
-		re: /(&lt;!--.*?--&gt;)/g,
+		re: /(&lt;!--[\s\S]*?--&gt;)/g,
 		index: 1
 	});
 
 	patterns.push({
 		token: "keyword",
-		re: /(&lt;[a-z\/].*?&gt;)/ig,
+		re: /(&lt;[a-z\/][\s\S]*?&gt;)/ig,
 		index: 1
 	});
 
@@ -19,5 +19,5 @@
 		index: 2
 	});
 
-	Syntax.register("html", patterns);
+	Syntax.register("xml", patterns);
 })();
